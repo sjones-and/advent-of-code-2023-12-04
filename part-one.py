41b6bc15cdce88b4e441ad0ad199acda09a31647
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-04 09:54:15 +0000

    Day four solution

--- part-one.py
+++ part-one.py
@@ -2,11 +2,16 @@
 
 import os
 
+def get_numbers(input):
+    return set([int(value.strip()) for value in input.split(' ') if len(value)>0])
+
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
 
-    answer = None
+    data = [line.split(':')[1].split('|') for line in data]
+    values = [len(set.intersection(get_numbers(value[0]), get_numbers(value[1]))) for value in data]
+    answer = sum([pow(2, value - 1) for value in values if value > 0])
 
     print(f'The answer is: {answer}')
 
--- part-two.py
+++ part-two.py
@@ -2,11 +2,28 @@
 
 import os
 
+def get_numbers(input):
+    return set([int(value.strip()) for value in input.split(' ') if len(value)>0])
+
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
+    card_win_count = {}
+    card_count = {}
+    for line in data:
+        [card_id, card_data] = line.split(':')
+        card_id = int(card_id[5:].strip())
+        card_data = card_data.split('|')
+        card_count[card_id] = 1
+        card_win_count[card_id] = len(set.intersection(get_numbers(card_data[0]), get_numbers(card_data[1])))
 
-    answer = None
+    for i in sorted(card_count.keys()):
+        win_count = card_win_count[i]
+        held_count = card_count[i]
+        for j in range(i + 1, i + 1 + win_count):
+            card_count[j] += held_count
+
+    answer = sum(card_count.values())
 
     print(f'The answer is: {answer}')
 
