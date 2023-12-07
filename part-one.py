dde1070ae83181bcc03f2625726c17831a4df3cb
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-07 22:24:08 +0000

    Add timing code

--- part-one.py
+++ part-one.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
 import os
+from time import perf_counter_ns
 
 def get_numbers(input):
     return set([int(value.strip()) for value in input.split(' ') if len(value)>0])
 
 def answer(input_file):
+    start = perf_counter_ns()
     with open(input_file, "r") as input:
         data = input.read().split('\n')
 
     data = [line.split(':')[1].split('|') for line in data]
     values = [len(set.intersection(get_numbers(value[0]), get_numbers(value[1]))) for value in data]
     answer = sum([pow(2, value - 1) for value in values if value > 0])
+    end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
+    print(f'{((end-start)/1000000):.2f} milliseconds')
 
 input_file = os.path.join(os.path.dirname(__file__), "input")
 answer(input_file)
--- part-two.py
+++ part-two.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+from time import perf_counter_ns
 
 def get_numbers(input):
     return set([int(value.strip()) for value in input.split(' ') if len(value)>0])
 
 def answer(input_file):
+    start = perf_counter_ns()
     with open(input_file, "r") as input:
         data = input.read().split('\n')
     card_win_count = {}
@@ -24,8 +26,10 @@ def answer(input_file):
             card_count[j] += held_count
 
     answer = sum(card_count.values())
+    end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
+    print(f'{((end-start)/1000000):.2f} milliseconds')
 
 input_file = os.path.join(os.path.dirname(__file__), "input")
 answer(input_file)
